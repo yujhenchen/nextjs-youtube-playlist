94beda40917d7810ef3f2b0c96c988969ea63381
yujhenchen <yujhen.chen93@gmail.com>
2024-03-27 23:37:05 +0800

    feat: use breadcrumbs

--- app/bands/[id]/layout.tsx
+++ app/bands/[id]/layout.tsx
@@ -1,9 +1,52 @@
-import React from "react";
+import React from 'react';
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import Link from 'next/link';
+
+const breadcrumbs = [
+  <Link
+    // underline='hover'
+    key='1'
+    color='inherit'
+    href='/'
+    // onClick={handleClick}
+  >
+    A
+  </Link>,
+  <Link
+    // underline='hover'
+    key='2'
+    color='inherit'
+    href='/material-ui/getting-started/installation/'
+    // onClick={handleClick}
+  >
+    B
+  </Link>,
+  <Link
+    // underline='hover'
+    key='3'
+    color='inherit'
+    href='/material-ui/getting-started/'
+    // onClick={handleClick}
+  >
+    C
+  </Link>
+];
 
 export default function BandPageLayout({
-  children,
+  children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <div>{children}</div>;
+  return (
+    <div>
+      <Breadcrumbs
+        separator={<NavigateNextIcon fontSize='small' />}
+        aria-label='breadcrumb'
+      >
+        {breadcrumbs}
+      </Breadcrumbs>
+      {children}
+    </div>
+  );
 }
